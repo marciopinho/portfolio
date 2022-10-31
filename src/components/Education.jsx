8cb82dc92dbea792a43c655473f2dc66db50a75d
Mars Pinho <marciopinho@gmail.com>
2022-10-31 14:20:38 -0300

    changed flex properties for desktop

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -8,40 +8,42 @@ export default function Education() {
     return (
         <div className="education">
 
-            <h1>education.</h1>
-            <div className="education--instance" data-aos="fade-up">
-                <img src={Wagon} alt="Le Wagon logo" />
-                <div>
-                    <h2>Le Wagon</h2>
-                    <h3 className='education--degree'>Full-stack Web Development</h3>
-                    <h3>2022</h3>
+            <h1 data-aos="fade-up">education.</h1>
+
+            <div className="education--entries">
+                <div className="education--entry" data-aos="fade-up">
+                    <img src={Wagon} alt="Le Wagon logo" />
+                    {/* <div> */}
+                        <h2>Le Wagon</h2>
+                        <h3 className='education--degree'>Full-stack Web Development</h3>
+                        <h3>2022</h3>
+                    {/* </div> */}
+                </div>
+
+                <div className="education--entry" data-aos='fade-up'>
+                    <img src={Polito} alt="" />
+                    <h2>Politecnico di Torino</h2>
+                    <h3 className='education--degree'>Systemic Design, MSc</h3>
+                    <h3>2011 - 2013</h3>
+                </div>
+
+                <div className="education--entry" data-aos='fade-up'>
+                    <img src={Uemg} alt="" />
+                    {/* <h2>Universidade do Estado de Minas Gerais</h2> */}
+                    <h2>UEMG</h2>
+                    <h3 className='education--degree'>Graphic Design, BSc</h3>
+                    <h3>2006 - 2011</h3>
+                </div>
+
+                <div className="education--entry" data-aos='fade-up'>
+                    <img src={Ufmg} alt="" />
+                    {/* <h2>Universidade Federal de Minas Gerais</h2> */}
+                    <h2>UFMG</h2>
+                    <h3 className='education--degree'>Business Administration, BSc</h3>
+                    <h3>2006 - 2011</h3>
                 </div>
             </div>
-
-            <div>
-
-            </div>
-
-            <div className="education--instance" data-aos='fade-up'>
-                <img src={Polito} alt="" />
-                <h2>Politecnico di Torino</h2>
-                <h3 className='education--degree'>Systemic Design, MSc</h3>
-                <h3>2011 - 2013</h3>
-            </div>
-
-            <div className="education--instance" data-aos='fade-up'>
-                <img src={Uemg} alt="" />
-                <h2>Universidade do Estado de Minas Gerais</h2>
-                <h3 className='education--degree'>Graphic Design, BSc</h3>
-                <h3>2006 - 2011</h3>
-            </div>
-
-            <div className="education--instance" data-aos='fade-up'>
-                <img src={Ufmg} alt="" />
-                <h2>Universidade Federal de Minas Gerais</h2>
-                <h3 className='education--degree'>Business Administration, BSc</h3>
-                <h3>2006 - 2011</h3>
-            </div>
+            
 
         </div>
     )
