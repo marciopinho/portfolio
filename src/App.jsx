54a94e4979b252580536e33cd10020404a3ec6e0
Mars Pinho <marciopinho@gmail.com>
2022-10-31 12:38:27 -0300

    alternating backgrounds

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { fab } from '@fortawesome/free-brands-svg-icons'
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import Education from './components/Education';
+import Experience from './components/Experience';
 
 
 
@@ -29,6 +30,7 @@ function App() {
 
       <About />
       <Education />
+      <Experience />
       <Contact />
     </div>
   )
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,44 @@
 export default function Experience() {
     return (
         <div className="experience">
-            
+            <h1>experience.</h1>
+
+            <div className="experience--instance">
+                <div>
+                    <h2>Zinger</h2>
+                    <h3 className='experience--role'>Founder</h3>
+                    <h3>2018 - 2022</h3>
+                    <p>Founder and manager of a micro production of craft ginger ale, supplying local bars and restaurants. Activities included managing small teams (brewing and packaging), cashflow, sales, equipment and supplies.</p>
+                </div>
+            </div>
+
+            <div className="experience--instance">
+                <div>
+                    <h2>Graphic Designer</h2>
+                    <h3 className='experience--role'>Freelancer</h3>
+                    <h3>2014 - 2022</h3>
+                    <p>Freelance designer for print and digital media.</p>
+                </div>
+            </div>
+
+            <div className="experience--instance">
+                <div>
+                    <h2>Real Estate Developer</h2>
+                    <h3 className='experience--role'>Harpa</h3>
+                    <h3>2016 - 2018</h3>
+                    <p>Active role in an affordable housing project consisting of four households, managing cashflow, purchase and inventory of building mater</p>
+                </div>
+            </div>
+
+            <div className="experience--instance">
+                <div>
+                    <h2>Intern</h2>
+                    <h3 className='experience--role'>Construtora Vesper</h3>
+                    <h3>2014</h3>
+                    <p>Internship in a mid-size construction company, providing support with administrative tasks in market research, accounting and suppliers management.</p>
+                </div>
+            </div>
+
         </div>
     )
 }
