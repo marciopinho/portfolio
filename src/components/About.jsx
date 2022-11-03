be632b1d575b93356bb4ad9ade69c89784482837
Mars Pinho <marciopinho@gmail.com>
2022-11-03 13:56:03 -0300

    yello avatar

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,9 @@
+import profileImg from '../assets/pixelated-avatar-medium-yellow.jpg'
+
 export default function About() {
     return (
         <div className="about">
-            <img src="https://res.cloudinary.com/dkearav60/image/upload/v1666981964/profile-pictures/pixelated-avatar-medium_wfhyhy.jpg" alt="profile picture" id="about--profile-picture" data-aos="fade-up"/>
+            <img src={profileImg} alt="profile picture" id="about--profile-picture" data-aos="fade-up"/>
             <div className="about--text">
                 <h1 className="about--title" data-aos="fade-up">about.</h1>
                 <p className="about--content" data-aos="fade-up">I'm a full-stack web developer based in Brazil. I've worked mostly with Ruby on Rails, JavaScript, CSS, HTML and PostgreSQL, but I’m in a permanent state of learning new tools and delving deeper into the ones I'm comfortable with.</p>
