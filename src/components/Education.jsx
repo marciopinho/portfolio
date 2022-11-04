9ba49fd52db41c22e01015b8c87bcdc223c0ac6f
Mars Pinho <marciopinho@gmail.com>
2022-11-04 20:38:07 -0300

    deleted some redundant css

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,40 +6,35 @@ import Wagon from '../assets/wagon_black.png'
 
 export default function Education() {
     return (
-        <div className="education">
-
+        <div className="listings">
             <h1 data-aos="fade-up">education.</h1>
 
-            <div className="education--entries">
-                <div className="education--entry" data-aos="fade-up">
+            <div>
+                <div className="listing-card" data-aos="fade-up">
                     <img src={Wagon} alt="Le Wagon logo" />
-                    {/* <div> */}
-                        <h2>Le Wagon</h2>
-                        <h3 className='education--degree'>Full-stack Web Development</h3>
-                        <h3>2022</h3>
-                    {/* </div> */}
+                    <h2>Le Wagon</h2>
+                    <h3>Full-stack Web Development</h3>
+                    <h3>2022</h3>
                 </div>
 
-                <div className="education--entry" data-aos='fade-up'>
+                <div className="listing-card" data-aos='fade-up'>
                     <img src={Polito} alt="" />
                     <h2>Politecnico di Torino</h2>
-                    <h3 className='education--degree'>Systemic Design, MSc</h3>
+                    <h3>Systemic Design, MSc</h3>
                     <h3>2011 - 2013</h3>
                 </div>
 
-                <div className="education--entry" data-aos='fade-up'>
+                <div className="listing-card" data-aos='fade-up'>
                     <img src={Uemg} alt="" />
-                    {/* <h2>Universidade do Estado de Minas Gerais</h2> */}
                     <h2>UEMG</h2>
-                    <h3 className='education--degree'>Graphic Design, BSc</h3>
+                    <h3>Graphic Design, BSc</h3>
                     <h3>2006 - 2011</h3>
                 </div>
 
-                <div className="education--entry" data-aos='fade-up'>
+                <div className="listing-card" data-aos='fade-up'>
                     <img src={Ufmg} alt="" />
-                    {/* <h2>Universidade Federal de Minas Gerais</h2> */}
                     <h2>UFMG</h2>
-                    <h3 className='education--degree'>Business Administration, BSc</h3>
+                    <h3>Business Administration, BSc</h3>
                     <h3>2006 - 2011</h3>
                 </div>
             </div>
