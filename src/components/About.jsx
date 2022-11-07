c64c48b46cc7d580d5941e9c72d40d4910a4e262
Mars Pinho <marciopinho@gmail.com>
2022-11-07 12:30:15 -0300

    set margin for about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
         <div className="about">
             <img src={profileImg} alt="profile picture" id="about--profile-picture" data-aos="fade-up"/>
             <div className="about--text">
-                <h1 className="about--title" data-aos="fade-up">about.</h1>
+                <h1 className="about--title" data-aos="fade-up">.about</h1>
                 <p className="about--content" data-aos="fade-up">I'm a full-stack web developer based in Brazil. I've worked mostly with Ruby on Rails, JavaScript, CSS, HTML and PostgreSQL, but I’m in a permanent state of learning new tools and delving deeper into the ones I'm comfortable with.</p>
             </div>
         </div>
