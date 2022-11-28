d075e34aa7f6e59726a5f115447ae916dcd50eeb
Mars Pinho <marciopinho@gmail.com>
2022-11-28 17:52:13 -0300

    created data files for the components

---
+++ projects_data.js
@@ -0,0 +1,40 @@
+export default [
+  {
+    id: 1,
+    title: "FestCheck",
+    subtitle: "Map your listening habits and find gigs to go",
+    date: "Ongoing",
+    description: "FestCheck picks up where Partify left off. The application is being further developed and improved upon, before being released for real-world testing.",
+  },
+  {
+    id: 2,
+    title: "Partify",
+    subtitle: "Streamlining nightlife recommendations.",
+    date: "2022",
+    description: "Partify is a web app that models a user's music taste from data fetched via Spotify's API and recommends matching events and venues. Partify is not online as of this writing, but you can read about it <a href='https://marciopinho.medium.com/partify-streamlining-nightlife-recommendations-aeb019ca273a' target='_blank'><strong>here</strong></a>.</p>",
+    link: "https://marciopinho.medium.com/partify-streamlining-nightlife-recommendations-aeb019ca273a",
+  },
+  {
+    id: 3,
+    title: "ZapGo",
+    subtitle: "Send WhatsApp messages to unsaved contacts.",
+    date: "2022",
+    description: "ZapGo is a simple and useful application which allows WhatsApp messages to be sent to recipients who are not on the sender's contact list.",
+    link: "https://zapgo.online",
+  },
+  {
+    id: 4,
+    title: "WorkSpace",
+    subtitle: "B2P and P2P coworking spaces.",
+    date: "2022",
+    description: "A Ruby on Rails platform to connect businesses or individuals who have available coworking spaces and potential customers. The application was developed as a project for Le Wagon's web development bootcamp.",
+  },
+  {
+    id: 5,
+    title: "Portfolio",
+    subtitle: "My website.",
+    date: "2022",
+    description: "The page you're in is my first ReactJS project. It was created with Vite, and the code is available on my GitHub page.",
+    link: "https://github.com/marciopinho/portfolio",
+  }
+]
